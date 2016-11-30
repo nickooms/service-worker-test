8bcf9908b834eb588e0dd8ad6c4544162168b0c9
Nick Ooms <oomsni@gmail.com>
2016-11-30 23:39:59 +0100

    initial

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'service-worker-test-cache-v1';
+
+const urlsToCache = [
+  '/',
+  '/main.css',
+  '/index.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME)
+    .then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    }));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request)
+    .then(response => {
+      if (response) return response;
+      const fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    }));
+});
