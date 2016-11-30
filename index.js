8b0039d7c963d82d46892ba2e2338c21755780b2
Nick Ooms <oomsni@gmail.com>
2016-11-30 23:52:40 +0100

    added github pages path service-worker-test/sw.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(registration => { 
+  navigator.serviceWorker.register('/service-worker-test/sw.js').then(registration => { 
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
   }).catch(err => {
     console.log('ServiceWorker registration failed: ', err);
